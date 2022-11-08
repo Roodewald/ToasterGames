baa420d63980c810ddf9634a1cf1a2809121d511
Roodewald <pavel999977@gmail.com>
2022-11-08 23:14:46 +0500

    Server rpcs

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,3 +1,4 @@
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -10,32 +11,44 @@ public class PlayerNetwork : NetworkBehaviour
     private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(new MyCustomData { _int= 56, _bool = true}
     ,NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
 
-    public struct MyCustomData
+    public struct MyCustomData: INetworkSerializable
     {
         public int _int;
         public bool _bool;
+        public FixedString128Bytes message;
 
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+            serializer.SerializeValue(ref message);
+        }
     }
 
     public override void OnNetworkSpawn()
     {
         randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) =>
-				Debug.Log(OwnerClientId + "; RandomNumber: " + newValue._int + "; "+ newValue._bool);
+				Debug.Log(OwnerClientId + "; RandomNumber: " + newValue._int + "; "+ newValue._bool + newValue.message);
 	}
 
     private void Update()
     {
-
         if (!IsOwner) return;
         Move();
 
         if (Input.GetKeyDown(KeyCode.T))
         {
+            TestServerRpc();
+
+
+            /*
             randomNumber.Value = new MyCustomData
             {
                 _int = 10,
-                _bool = false
+                _bool = false,
+                message = "Hi world"
             };
+            */
         }
     }
 
@@ -47,5 +60,11 @@ public class PlayerNetwork : NetworkBehaviour
         moveDiraction = transform.forward * verticalMovement + transform.right * horizontalMovement;
 
         transform.position += moveDiraction * 3 *Time.deltaTime;
+    }
+
+    [ServerRpc]
+    private void TestServerRpc()
+    {
+        Debug.Log("TestServerRpc" + OwnerClientId);
     }
 }
