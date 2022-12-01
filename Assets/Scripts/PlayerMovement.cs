372e9bc77c66fc6bea7adbda705d392d5264bbec
Roodewald <pavel999977@gmail.com>
2022-12-01 12:23:18 +0500

    0.1.1-b
    Add camera controll

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,34 +9,33 @@ public class PlayerMovement : NetworkBehaviour
 	[SerializeField] private CharacterController controller;
 	float xRotation = 0f;
 	
-	public Transform playerCamera;
+	public GameObject playerCamera;
 	public float mouseSensetiviy = 100f;
 	public float speed = 12f;
 
-	private void Awake()
+	public override void OnNetworkSpawn()
 	{
 		if (IsOwner)
 		{
-			playerCamera.gameObject.SetActive(false);
+			playerCamera.SetActive(true);
 		}
 	}
 
 	private void Update()
 	{
-
 		if (!IsOwner) return;
 		Look();
 		Move();
 	}
 	private void Look()
 	{
-		float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensetiviy * Time.deltaTime;
-		float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensetiviy * Time.deltaTime;
+		float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensetiviy;
+		float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensetiviy;
 
 		xRotation -= mouseY;
 		xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-		playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+		playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 		transform.Rotate(Vector3.up * mouseX);
 	}
 	private void Move()
