9ecd64d13b9e67c6176e0fcd1de3ccbf28fc5b49
Roodewald <pavel999977@gmail.com>
2022-12-04 13:01:34 +0500

    Add portUi

--- Assets/Scripts/UINetworkManager.cs
+++ Assets/Scripts/UINetworkManager.cs
@@ -3,17 +3,30 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using Unity.Netcode.Transports.UTP;
+using System;
 
 public class UINetworkManager : MonoBehaviour
 {
 	[SerializeField] private Button serverButton;
 	[SerializeField] private Button HostButton;
 	[SerializeField] private Button clientButton;
+	[SerializeField] private TMP_InputField IP;
+	[SerializeField] private TMP_InputField port;
+	[SerializeField] private Button applyButton;
+	[SerializeField] private UnityTransport unityTransport;
 
 	private void Awake()
 	{
+		
 		serverButton.onClick.AddListener(() => NetworkManager.Singleton.StartServer());
 		HostButton.onClick.AddListener(() => NetworkManager.Singleton.StartHost());
 		clientButton.onClick.AddListener(() => NetworkManager.Singleton.StartClient());
+	}
+	public void applyIpAndPort()
+	{
+		unityTransport.ConnectionData.Address = IP.text;
+		unityTransport.ConnectionData.Port = Convert.ToUInt16(port.text);
 	}
 }
