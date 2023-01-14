076a538ee42745fd1dbf8dc18b2a438a5b7e760d
Roodewald <pavel999977@gmail.com>
2023-01-14 22:42:53 +0500

    0.0.3.dev

--- Assets/Scripts/TestLobby.cs
+++ Assets/Scripts/TestLobby.cs
@@ -1,10 +1,14 @@
 using QFSW.QC;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Lobbies;
 using Unity.Services.Lobbies.Models;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
 
 public class TestLobby : MonoBehaviour
@@ -166,5 +170,56 @@ public class TestLobby : MonoBehaviour
 						{ "PlayerName", new PlayerDataObject(PlayerDataObject.VisibilityOptions.Public, playerName) }
 					}
 		};
+	}
+
+	[Command]
+	private async void CreateRelay()
+	{
+		try
+		{
+			Allocation allocation = await RelayService.Instance.CreateAllocationAsync(3);
+
+			string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+			Debug.Log(joinCode);
+
+			NetworkManager.Singleton.GetComponent<UnityTransport>().SetHostRelayData(
+				
+				allocation.RelayServer.IpV4,
+				(ushort)allocation.RelayServer.Port,
+				allocation.AllocationIdBytes,
+				allocation.Key,
+				allocation.ConnectionData
+			);
+			NetworkManager.Singleton.StartHost();
+		}
+		catch (RelayServiceException e)
+		{
+			Debug.Log(e);
+		}
+		
+	}
+
+	[Command]
+	private async void JoinRealy(string joinCode)
+	{
+		try
+		{
+			JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+
+			NetworkManager.Singleton.GetComponent<UnityTransport>().SetClientRelayData(
+				joinAllocation.RelayServer.IpV4,
+				(ushort)joinAllocation.RelayServer.Port,
+				joinAllocation.AllocationIdBytes,
+				joinAllocation.Key,
+				joinAllocation.ConnectionData,
+				joinAllocation.HostConnectionData
+			);
+			NetworkManager.Singleton.StartClient();
+		}
+		catch (RelayServiceException e)
+		{
+			Debug.Log(e);
+		}
+		
 	}
 }
